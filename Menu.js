5d3efdc69c8f06a676f2d86364da5311f09baf10
Johnny <fonsecajanjao@gmail.com>
2025-03-06 11:01:52 +0000

    Menu em desenvolvimento

---
+++ Menu.js
@@ -0,0 +1,90 @@
+let width ;
+let height;
+
+let di = '2025-01-01';
+let df = '2025-12-31';
+
+class Menu extends Phaser.Scene {
+
+    constructor() {
+        super('Menu');
+    }
+
+    preload() {
+        this.load.image('background','assets/background.png');
+        this.load.image('titulo','assets/titulo.png');
+        this.load.image('btCreditos','assets/bt-creditos.png');
+        this.load.image('btInstrucoes', 'assets/bt-instrucoes.png');
+        this.load.image('btTop', 'assets/bt-top.png');
+        this.load.image('btPVP', 'assets/bt-level0.png');
+        this.load.image('btLvl1', 'assets/bt-level1.png');
+        this.load.image('btLvl2', 'assets/bt-level2.png');
+        this.load.image('btLvl3', 'assets/bt-level3.png');
+        this.load.image('lapis', 'assets/lapis.png');
+    }
+
+    create() {
+        //Variáveis a ser usadas para adicionar os sprites
+        width = game.config.width;
+        height = game.config.height;
+
+        //Background
+        this.background = this.add.sprite(0.5 * width, 0.5 * height,'background');     
+        this.background.setScale(1.5);
+        
+        //Titulo
+        this.titulo = this.add.sprite(0.6 * width, 0.16 * height,'titulo');
+        this.titulo.setScale(1.6);
+
+        //Creditos
+        this.btCreditos = this.add.sprite(0.94 * width, 0.90 * height, 'btCreditos');
+        this.btCreditos.setScale(1.2);
+        this.btCreditos.setInteractive({ useHandCursor: true });
+
+        //Instrucoes
+        this.btInstrucoes = this.add.sprite(0.94 * width, 0.73 * height, 'btInstrucoes');
+        this.btInstrucoes.setScale(1.2);
+        this.btInstrucoes.setInteractive({ useHandCursor: true });
+
+        //Top
+        this.btTop = this.add.sprite(0.94 * width, 0.56 * height, 'btTop');
+        this.btTop.setScale(1.2);
+        this.btTop.setInteractive({useHandCursor: true});
+
+        //PVP
+        this.btPVP = this.add.sprite(0.6 * width, 0.35 * height, 'btPVP');
+        this.btPVP.setScale(1);
+        this.btPVP.setInteractive({useHandCursor: true});
+
+        //Level1
+        this.btLvl1 = this.add.sprite(0.6 * width, 0.53 * height, 'btLvl1');
+        this.btLvl1.setScale(1);
+        this.btLvl1.setInteractive({useHandCursor: true});
+
+        //Level2
+        this.btLvl2 = this.add.sprite(0.6 * width, 0.71 * height, 'btLvl2');
+        this.btLvl2.setScale(1);
+        this.btLvl2.setInteractive({useHandCursor: true});
+        
+        //level3
+        this.btLvl3 = this.add.sprite(0.6 * width, 0.89 * height, 'btLvl3');
+        this.btLvl3.setScale(1);
+        this.btLvl3.setInteractive({useHandCursor: true});
+
+        //Lapis
+        this.lapis = this.add.sprite(0.47 * width, 0.66 * height, 'lapis');
+        this.lapis.setScale(1.3);
+
+        //BT Logic
+        //BT Highlight
+        this.input.on('gameobjectover',function(pointer, gameObject) {
+            gameObject.displayHeight += 5;
+            gameObject.displayWidth += 5;
+        },this);
+        this.input.on('gameobjectout',function(pointer, gameObject) {
+            gameObject.displayHeight -= 5;
+            gameObject.displayWidth -= 5;
+        },this);
+    }
+
+}
